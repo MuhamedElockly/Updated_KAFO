7d2546eac7d86c2ecf37b4b6c1c0c924b4d062b1
dell <elocklymuhamed@gmail.com>
2025-07-29 05:13:58 +0300

    add signalR to make statistics Real Time

---
+++ KAFO.ASPMVC/Hubs/StatisticsHub.cs
@@ -0,0 +1,25 @@
+﻿using KAFO.Domain.ViewModels;
+using Microsoft.AspNetCore.SignalR;
+namespace KAFO.ASPMVC.Hubs
+{
+	public class StatisticsHub : Hub
+	{
+		public async Task SendStatisticsUpdate(StatisticsMessages statisticsMessages)
+		{
+			// Broadcast the message to all connected clients
+			await Clients.All.SendAsync("ReceiveStatisticsUpdate", statisticsMessages);
+		}
+
+		public override Task OnConnectedAsync()
+		{
+			// Logic when a client connects, if needed
+			return base.OnConnectedAsync();
+		}
+
+		public override Task OnDisconnectedAsync(Exception exception)
+		{
+			// Logic when a client disconnects, if needed
+			return base.OnDisconnectedAsync(exception);
+		}
+	}
+}
